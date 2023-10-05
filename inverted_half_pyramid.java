ff41ed433aeb09e05f77c09b0cdadac3aac4bd7a
Ashutosh Singh <101076940+ashutoshsingh251222@users.noreply.github.com>
2023-10-06 00:46:12 +0530

    Add files via upload

---
+++ inverted_half_pyramid.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class inverted_half_pyramid {
+    public static void mian(String args[]) {
+        int n = 4;
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
