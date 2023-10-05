ae8cdefdbc8e59c3d9d54ee8ab27a6704e764369
Ashutosh Singh <101076940+ashutoshsingh251222@users.noreply.github.com>
2023-10-06 00:45:09 +0530

    Add files via upload

---
+++ 180*inverted_pyramid.java
@@ -0,0 +1,22 @@
+import java.util.*;
+class Inverted_java
+{
+    public static void main(String areg[])
+    {
+        Scanner sc= new Scanner(System.in);
+        System.out.println("Enter the number of Rows: ");
+        int n=sc.nextInt();
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=1;j<=(n-i);j++)
+            {
+                System.out.print(" ");
+            }
+            for (int j=1;j<=i;j++){
+                    System.out.print("*");
+            }
+            System.out.println();
+        }
+
+    }
+}
